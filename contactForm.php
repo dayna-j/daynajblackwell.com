c65ee1ecfd873d5034760b3eeb5edd9794e48e6a
Dayna Blackwell <daynajblackwell@gmail.com>
2018-05-23 19:57:42 -0700

    Created contactForm.php
    Created php script to submit email to server.

---
+++ contactForm.php
@@ -0,0 +1,21 @@
+<?php
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $from = 'From: www.daynajblackwell.com contact form'; 
+    $to = 'dayna-j@daynajblackwell.com'; 
+    $subject = 'Contact form submission';
+
+		$body = "E-Mail: $email\n Message:\n $message";
+
+
+
+		if ($_POST['submit']) {
+			if (mail ($to, $subject, $body, $from)) { 
+        echo '<p>Your message has been sent!</p>';
+    } else { 
+        echo '<p>Something went wrong, go back and try again!</p>'; 
+    }
+}	
+
+?>
